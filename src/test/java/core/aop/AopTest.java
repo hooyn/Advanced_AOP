ba327eca46b9775159fb82056e1685d5117e3ced
JungHoyun <hooyn_industry@naver.com>
2022-12-20 12:01:09 +0900

    feat: 스프링 AOP 구현 #5 - 어드바이스 순서

--- src/test/java/core/aop/AopTest.java
+++ src/test/java/core/aop/AopTest.java
@@ -2,10 +2,7 @@ package core.aop;
 
 import core.aop.order.OrderRepository;
 import core.aop.order.OrderService;
-import core.aop.order.app.AspectV1;
-import core.aop.order.app.AspectV2;
-import core.aop.order.app.AspectV3;
-import core.aop.order.app.AspectV4Pointcut;
+import core.aop.order.app.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,7 +13,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TaxAspect.class})
 public class AopTest {
 
     @Autowired
