dbb031e837214cc3c9edf267f674b5127fb280e9
JungHoyun <hooyn_industry@naver.com>
2022-12-20 13:54:21 +0900

    feat: 스프링 AOP 구현 #6 - 어드바이스 종류

---
+++ src/main/java/core/aop/order/app/AspectV6Advice.java
@@ -0,0 +1,52 @@
+package core.aop.order.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+
+//    @Around("core.aop.order.app.Pointcuts.orderAndService()")
+//    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+//
+//        try {
+//            //@Before
+//            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+//            Object result = joinPoint.proceed();
+//
+//            //@AfterReturning
+//            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+//            return result;
+//        } catch (Exception e) {
+//            //@AfterThrowing
+//            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+//            throw e;
+//        } finally {
+//            //@After
+//            log.info("[리소스 릴리즈 ] {}", joinPoint.getSignature());
+//        }
+//    }
+
+    @Before("core.aop.order.app.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "core.aop.order.app.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {} return={}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "core.aop.order.app.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[ex] {} message={}", ex);
+    }
+
+    @After(value = "core.aop.order.app.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
